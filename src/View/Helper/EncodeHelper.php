b340d402b83d4d3459cbf97d5ae49aeab2937ae6
Yasuaki Hamano <hamano@fusic.co.jp>
2016-11-24 15:40:22 +0900

    added the smartphone detector, and rich comments.

--- src/View/Helper/EncodeHelper.php
+++ src/View/Helper/EncodeHelper.php
@@ -1,31 +1,32 @@
 <?php
+
 namespace Garak\View\Helper;
 
 use Cake\View\Helper;
 use Cake\View\View;
-use Cake\Core\Configure;
-use Cake\ORM\Entity;
 use Cake\Event\Event;
 use Garak\Garak\Detector;
 
 /**
  * Escape helper
  * To convert special characters of variables to HTML entities
- * that is passeed to View object
+ * that is passeed to View object.
  */
 class EncodeHelper extends Helper
 {
     /**
-     * afterLayout description
-     * @param  Event  $Event      [description]
-     * @param  [type] $layoutFile [description]
-     * @return [type]             [description]
+     * afterLayout convert utf-8 to shift_jis.
+     *
+     * @param Event  $Event      [description]
+     * @param [type] $layoutFile [description]
+     *
+     * @return [type] [description]
      */
     public function afterLayout(Event $Event, $layoutFile)
     {
-        if (Detector::characterCode() !== "UTF-8") {
+        if (Detector::characterCode() !== 'UTF-8') {
             $content = $Event->subject->Blocks->get('content');
-            $encoded = mb_convert_encoding($content, "shift_jis", "utf-8");
+            $encoded = mb_convert_encoding($content, 'shift_jis', 'utf-8');
             $Event->subject->Blocks->set('content', $encoded);
         }
     }
